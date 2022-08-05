5397cebc4f27a28f88594f8fd5a72906330962c9
VinothJV10 <vinothjv10@gmail.com>
2022-08-06 01:49:16 +0530

    ohho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormPageComponent } from './form-page/form-page.component';
 
+import { FormsModule } from '@angular/forms';
+// import { AngularFireModule } from '@angular/fire';
+
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +24,12 @@ import { FormPageComponent } from './form-page/form-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form-page/form-page.component.ts
+++ src/app/form-page/form-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-page',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormPageComponent implements OnInit {
 
-  constructor() { }
+  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  contactForm: any = FormGroup;
+
+  isSubmit = false;
+  sumbitMessage = '';
+
+  submitted = false;
+
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.contactForm = this.formbuilder.group({
+      name: [null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      lname: [null, Validators.required],
+      // email: [null, [Validators.required, Validators.email]],
+      email: ['', Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])],
+      message: [null, Validators.required],
+    });
+  }
+  get f() { return this.contactForm.controls; }
+
+  submitData(e: any) {
+    this.submitted = true;
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+    console.log(e)
+    this.isSubmit = true;
+
+    this.sumbitMessage = " Sumbitted Successfully";
+    setTimeout(() => {
+      this.isSubmit = false;
+    }, 8000);
   }
 
 }
